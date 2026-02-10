22349fafb94470d82639a23b26dfd9fc36917e2d
Dinesh Kumar <179027812+dineshkumar8700@users.noreply.github.com>
2026-02-10 18:54:14 +0530

    Creates client.js for connecting to server

--- src/client/client.js
+++ src/client/client.js
@@ -1,1 +1,44 @@
-// client side code will come here
+const readFromServer = async (server) => {
+  const decoder = new TextDecoder();
+  const buffer = new Uint8Array(1024);
+
+  while (true) {
+    const bytesRead = await server.read(buffer);
+
+    if (bytesRead === null) return;
+
+    const msg = decoder.decode(buffer.slice(0, bytesRead));
+    console.log(msg);
+  }
+};
+
+const writeToServer = async (server) => {
+  const encoder = new TextEncoder();
+  const buffer = new Uint8Array(1024);
+
+  while (true) {
+    const bytesRead = await Deno.stdin.read(buffer);
+
+    if (bytesRead === null) return;
+
+    const msg = encoder.encode(buffer.slice(0, bytesRead));
+
+    await server.write(msg);
+  }
+};
+
+const connectToServer = async (port) => {
+  const conn = await Deno.connect({ port, transport: "tcp" });
+  return conn;
+};
+
+const main = async (args) => {
+  const [port = 8000] = args;
+  const server = await connectToServer(+port);
+  console.log("Conneted to server");
+
+  readFromServer(server);
+  writeToServer(server);
+};
+
+main(Deno.args);
