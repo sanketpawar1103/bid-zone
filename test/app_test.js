f14ec70a7a43e21bb7c2bd4c1ed998e5706f76ec
Dinesh Kumar <179027812+dineshkumar8700@users.noreply.github.com>
2026-02-10 17:58:27 +0530

    Completes project setup part
    * creates directories
    * adds deno tasks
    * adds dependencies

---
+++ test/app_test.js
@@ -0,0 +1,8 @@
+import { assertEquals } from "@std/assert";
+import { describe, it } from "@std/testing/bdd";
+
+describe("Sample", () => {
+  it("Sample test 1", () => {
+    assertEquals(1, 1);
+  });
+});
